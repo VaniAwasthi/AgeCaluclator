18095e9f3ca20184a6dc222071a40beba0bdc91f
Vani Awasthi <awasthivani95@gmail.com>
2023-05-01 17:34:21 +0530

    Adding new changes

---
+++ index.js
@@ -0,0 +1,42 @@
+//storing Elements in variable
+    var date = document.getElementById('date');
+    var calculate = document.getElementById('calculate');
+    var result = document.querySelector('.result');
+
+    // set maximum date to today
+    date.max = new Date().toISOString().split('T')[0];
+
+    function calculateAge() {
+      var today = new Date();
+      var birthDate = new Date(date.value);
+      var years;
+      if (today.getMonth() > birthDate.getMonth() || (today.getMonth() == birthDate.getMonth() && today.getDate() >= birthDate.getDate())) {
+        years = today.getFullYear() - birthDate.getFullYear();
+      }
+      else {
+        years = today.getFullYear() - birthDate.getFullYear() - 1;
+      }
+      var months;
+      if (today.getDate() >= birthDate.getDate()) {
+        months = today.getMonth() - birthDate.getMonth();
+      }
+      else if (today.getDate() < birthDate.getDate()) {
+        months = today.getMonth() - birthDate.getMonth() - 1;
+      }
+      months = months < 0 ? months + 12 : months;
+
+      var days;
+      // days of months in a year
+      var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      if (today.getDate() >= birthDate.getDate()) {
+        days = today.getDate() - birthDate.getDate();
+      } else {
+        days = today.getDate() - birthDate.getDate() + monthDays[birthDate.getMonth()];
+      }
+      result.innerHTML = `<p class="birthdate">You were born on ${birthDate.toDateString()}.</p>`;
+      result.innerHTML += `<p class="age">You are ${years} years, ${months} months and ${days} days old.</p>`;
+      if (months == 0 && days == 0) {
+        result.innerHTML += `<p class="wishing">Happy Birthday!</p>`;
+      }
+    }
+    calculate.addEventListener('click', calculateAge);
